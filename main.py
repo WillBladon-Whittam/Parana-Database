a7269d9ff068dcf5c0f7dc1676598c35c0ca76ba
WillBladon-Whittam <willbladon2.0@outlook.com>
2024-04-20 02:09:40 +0100

    Started added Option 2 (INCOMPLETE)  - When shopper id is entered, INSERT a basket if there is not already a basket from today  - When Option 2 is selected, show and prompt product categories -> products -> sellers
     ToDo:
     - When displaying sellers, list there prices next to them
     - Commit products to basket, need to first get the price it was sold at.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from sql import SqlWrapper
 from tabulate import tabulate
+import datetime
 
 from typing import Tuple, Union, List
 
 """
-Store database in the root directory as 'database'  or specify when calling SqlWrapper
+Store database in the root directory as 'database' or specify when calling SqlWrapper
+
+Links Used:
+https://learnpython.com/blog/print-table-in-python/
+
 """
 
 
@@ -16,7 +21,8 @@ class ParanaShopperSession:
         self.welcome()
         
         self.basket = self.get_basket_id()
-        
+        if not self.basket:
+            self.basket = self.create_basket()
         self.main_loop()
 
     def get_shopper_id(self) -> int:
@@ -35,10 +41,15 @@ class ParanaShopperSession:
         If there is basket created from today, use that basket and return the basket_id
         """
         return self.sql.select_query("SELECT basket_id "
-                                 "FROM shopper_baskets "
-                                 "WHERE shopper_id = ? AND DATE(basket_created_date_time) = DATE('now') "
-                                 "ORDER BY basket_created_date_time DESC "
-                                 "LIMIT 1", sql_parameters=(self.shopper_id,))
+                                     "FROM shopper_baskets "
+                                     "WHERE shopper_id = ? AND DATE(basket_created_date_time) = DATE('now') "
+                                     "ORDER BY basket_created_date_time DESC "
+                                     "LIMIT 1", sql_parameters=(self.shopper_id,), fetch_all=False)
+        
+    def create_basket(self) -> int:
+        self.sql.insert_query("INSERT INTO shopper_baskets (shopper_id, basket_created_date_time) "
+                              "VALUES (?, ?)", sql_parameters=(self.shopper_id, datetime.datetime.now().strftime("%Y-%m-%d"),))
+        return self.sql.cursor.lastrowid
     
     def welcome(self) -> None:
         """
@@ -80,8 +91,66 @@ class ParanaShopperSession:
         else:
             self.pretty_print(results=order_history, headers=["Order ID", "Order Date", "Product Description", "Seller", "Price", "Qty", "Status"])
             
-        self.main_loop()
+    def display_options(self, options: List[Tuple[str]]) -> None:
+        for i, option in enumerate(options, start=1):
+            print(f"{i}. \t{option[0]}")
+        print("\n")
         
+    def prompt_number(self, prompt: str, _range: Tuple[int, int] = None):
+        selected_option = None
+        if _range is None:
+            return int(input(prompt))
+        while selected_option not in range(_range[0], _range[1]):
+            selected_option = int(input(prompt))
+            if selected_option not in range(_range[0], _range[1]):
+                print("Invalid Value!\n")
+        return selected_option
+ 
+    def add_item(self):
+        """
+        Add item to the shoppers basket.
+        Choose product categories -> products -> sellers
+        """
+        product_categories = self.sql.select_query("SELECT category_description "
+                                                   "FROM categories")
+        
+        self.display_options(product_categories)
+        selected_category = self.prompt_number(prompt="Enter the number against the product category you want to choose: ", 
+                                               _range=(1, len(product_categories)))
+                
+        # Use an embeded SQL query to get the category id of the product selected, then query the products table for that category
+        products = self.sql.select_query("SELECT product_description "
+                                         "FROM products "
+                                         "WHERE category_id = (SELECT category_id "
+                                         "FROM categories "
+                                         "WHERE category_description = ?)", sql_parameters=product_categories[selected_category-1])
+        
+        self.display_options(products)
+        selected_product = self.prompt_number(prompt="Enter the number against the product you want to choose: ", 
+                                              _range=(1, len(products)))
+        
+        # Use an embeded SQL query to get the product id of the product selected, then query the products_sellers table for that product_id
+        sellers = self.sql.select_query("SELECT s.seller_name "
+                                        "FROM sellers s "
+                                        "INNER JOIN product_sellers ps ON s.seller_id = ps.seller_id "
+                                        "WHERE ps.product_id = (SELECT product_id "
+                                        "FROM products "
+                                        "WHERE product_description = ?)", sql_parameters=products[selected_product-1])
+        
+        
+        # ToDo: When displaying sellers, list there prices next to them    
+        self.display_options(sellers)
+        selected_seller = self.prompt_number(prompt="Enter the number against the seller you want to choose: ", 
+                                             _range=(1, len(sellers)))
+        
+        quantity = self.prompt_number(prompt="Enter the quantity of the selected product you want to buy: ")
+
+        # ToDo: Commit products to basket, need to first get the price it was sold at.        
+        # self.sql.insert_query("INSERT INTO basket_contents (basket_id, product_id, seller_id, quantity, price) "
+        #                       "VALUES (?, ?, ?, ?, ?)", sql_parameters=(self.basket, products[selected_product-1], sellers[selected_seller-1], quantity, ))
+        
+        print("Item Added")
+
     
     def main_menu(self) -> int:
         """
@@ -96,39 +165,40 @@ class ParanaShopperSession:
               "5.	Remove an item from your basket\n"
               "6.	Checkout\n"
               "7.	Exit\n")
-        return int(input())
+        return int(input("Select an option: "))
     
     def main_loop(self) -> None:
         """
         The main loop of the session
         """
-        # NOTE: match/case statements are python 3.10+
-        match self.main_menu():
-            
-            case 1:
-                self.display_order_history()
-            
-            case 2:
-                raise NotImplementedError("Add an item to your basket")
-            
-            case 3:
-                raise NotImplementedError("View your basket")
-            
-            case 4:
-                raise NotImplementedError("Change the quantity of an item in your basket")
-            
-            case 5:
-                raise NotImplementedError("Remove an item from your basket")
-            
-            case 6:
-                raise NotImplementedError("Checkout")
-            
-            case 7:
-                self.sql.close()
-                quit()
+        while True:
+            # NOTE: match/case statements are python 3.10+
+            match self.main_menu():
                 
-            case _:
-                print("Invalid Value")  # Close program?
+                case 1:
+                    self.display_order_history()
+                
+                case 2:
+                   self.add_item()
+                
+                case 3:
+                    raise NotImplementedError("View your basket")
+                
+                case 4:
+                    raise NotImplementedError("Change the quantity of an item in your basket")
+                
+                case 5:
+                    raise NotImplementedError("Remove an item from your basket")
+                
+                case 6:
+                    raise NotImplementedError("Checkout")
+                
+                case 7:
+                    self.sql.close()
+                    quit()
+                    
+                case _:
+                    print("Invalid Value")  # Close program?
                 
 
 if __name__ == "__main__":
--- sql.py
+++ sql.py
@@ -14,12 +14,13 @@ class SqlWrapper:
     def execute_query(self, sql_query: str, sql_parameters: Tuple[str, int] = tuple()) -> None:
         self.cursor.execute(sql_query, sql_parameters)
         
-    def select_query(self, sql_query, sql_parameters: Tuple[str, int] = tuple(), fetch_all: bool = True,):
+    def select_query(self, sql_query, sql_parameters: Tuple[str, int] = tuple(), fetch_all: bool = True):
         """
         Creates a SELECT query
 
         Args:
             sql_query: An SQL Query to execute
+            sql_parameters: Parameters for an SQL query
             fetch_all: If set to True, fetches all the rows returned, if set to False returns only 1
         """
         self.execute_query(sql_query, sql_parameters)
@@ -28,6 +29,19 @@ class SqlWrapper:
         else:
             return self.cursor.fetchone()
         
+    def insert_query(self, sql_query, sql_parameters: Tuple[str, int] = tuple(), commit=True):
+        """
+        Creates a INSERT query
+
+        Args:
+            sql_query: An SQL Query to execute
+            sql_parameters: Parameters for an SQL query
+            commit: Commit changes to database immediatly
+        """
+        self.execute_query(sql_query, sql_parameters)
+        if commit:
+            self.sql.db.commit()
+ 
     def close(self) -> None:
         self.db.close()
     
